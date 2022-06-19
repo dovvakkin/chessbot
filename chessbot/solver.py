0c1ba93e2fde62dd2815dff53d660f69304c00a5
IlidanNaga <max.korn@bk.ru>
2022-06-19 23:49:53 +0300

    Add docstrings (solver)

--- chessbot/solver.py
+++ chessbot/solver.py
@@ -1,7 +1,9 @@
+"""A module to launch Stockfish engine wrapper."""
+
 from stockfish import Stockfish
 
-default_path = 'chessbot\stockfish_15_win_x64_avx2\stockfish_15_x64_avx2.exe'
-default_parameters = {
+DEFAULT_PATH = 'chessbot\stockfish_15_win_x64_avx2\stockfish_15_x64_avx2.exe'
+DEFAULT_PARAMS = {
     "Contempt": 0,
     "Min Split Depth": 0,
     "Threads": 4,
@@ -19,7 +21,8 @@ default_parameters = {
 
 class Solver():
     """
-    A class to represent a wrapper of a stockfish solver
+    A class to represent a wrapper of a stockfish solver.
+
     ...
     Attributes:
     -----------
@@ -30,21 +33,22 @@ class Solver():
     """
 
     def __init__(self, path=None, depth=15, params=None):
-
+        """Initialise Stockfish wrapper."""
         if path is None:
-            path = default_path
+            path = DEFAULT_PATH
         if params is None:
-            params = default_parameters
+            params = DEFAULT_PARAMS
 
         self.solver = Stockfish(path=path, depth=depth, parameters=params)
 
     def make_step(self, fen):
-        """
-        Predicts best possible move for opposing team (PvE)
-        """
-
+        """Predict best possible move for black team."""
         if not self.solver.is_fen_valid(fen):
             return None
 
         self.solver.set_fen_position(fen)
         return self.solver.get_best_move()
+
+    def check_fen(self, fen):
+        """Check if there is a possible move from current state."""
+        return self.solver.is_fen_valid(fen)
