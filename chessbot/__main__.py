261c8cac410cf451d9c264dea2e09b1e2ce2e158
IlidanNaga <max.korn@bk.ru>
2022-06-20 00:18:47 +0300

    Fix (partially) lose condition

--- chessbot/__main__.py
+++ chessbot/__main__.py
@@ -1,4 +1,4 @@
-"""Module with Telegram bot's logic and program's mainloop"""
+"""Module with Telegram bot's logic and program's mainloop."""
 
 import time
 import string
@@ -105,9 +105,9 @@ def handle_query(call):
     if current_games[chat_id].has_prev_cell():
         current_games[chat_id].set_move_to(move)
         move_check = current_games[chat_id].check_move_valid()
-        print(move_check)
+        #print(move_check)
         if move_check == 1:
-            # обрабатывать ход
+            # Checking turn
             current_games[chat_id].clear_accum()
             img = open("chessbot/Current_game/board.png", 'rb')
             bot.edit_message_media(
@@ -122,7 +122,7 @@ def handle_query(call):
                 message_id=call.message.message_id,
                 reply_markup=make_keyboard()
             )
-            time.sleep(5) # to give player time to
+            time.sleep(1) # to give player time to
             #print(current_games[chat_id].chess.fen)
             move = stockfish_solver.make_step(current_games[chat_id].chess.fen)
             if move is None:
@@ -137,21 +137,34 @@ def handle_query(call):
 
                 bot_move_check = current_games[chat_id].check_move_valid()
                 if bot_move_check == 1:
-                    current_games[chat_id].clear_accum()
-                    img = open("chessbot/Current_game/board.png", 'rb')
-                    bot.edit_message_media(
-                        chat_id=call.message.chat.id,
-                        media=types.InputMediaPhoto(media=img),
-                        message_id=call.message.message_id,
-                        reply_markup=make_keyboard()
-                    )
-                    bot.edit_message_caption(
-                        chat_id=call.message.chat.id,
-                        caption=_("Сделай следующий ход"),
-                        message_id=call.message.message_id,
-                        reply_markup=make_keyboard()
-                    )
-                    current_games[chat_id].prev_state = 0
+
+                    # Ход бота закончился
+                    fen = current_games[chat_id].chess.fen
+                    if not stockfish_solver.check_fen(fen):
+                        bot.edit_message_caption(
+                            chat_id=call.message.chat.id,
+                            caption=_("Поражение((("),
+                            message_id=call.message.message_id
+                        )
+                        current_games[chat_id].clear_accum()
+
+                    else:
+                        # Preparation for next turn
+                        current_games[chat_id].clear_accum()
+                        img = open("chessbot/Current_game/board.png", 'rb')
+                        bot.edit_message_media(
+                            chat_id=call.message.chat.id,
+                            media=types.InputMediaPhoto(media=img),
+                            message_id=call.message.message_id,
+                            reply_markup=make_keyboard()
+                        )
+                        bot.edit_message_caption(
+                            chat_id=call.message.chat.id,
+                            caption=_("Сделай следующий ход"),
+                            message_id=call.message.message_id,
+                            reply_markup=make_keyboard()
+                        )
+                        current_games[chat_id].prev_state = 0
                 elif bot_move_check == 0:
                     bot.edit_message_caption(
                         chat_id=call.message.chat.id,
